b09c17118766f1c68a1534c34fbf299dd852196f
bdafahim <badrudduza20@student.sust.edu>
2020-06-12 20:14:37 +0600

    Dynamic navigations options

--- screens/CatagoryMealScreen.js
+++ screens/CatagoryMealScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { CATEGORIES } from "../data/dummy-data";
+import Colors from "../constants/Colors";
 
 const CategoryMealScreen = (props) => {
     const catId = props.navigation.getParam("categoryId");
@@ -19,6 +20,19 @@ const CategoryMealScreen = (props) => {
             </TouchableOpacity>
         </View>
     );
+};
+
+CategoryMealScreen.navigationOptions = (navigationData) => {
+    const catId = navigationData.navigation.getParam("categoryId");
+    const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+
+    return {
+        headerTitle: selectedCategory.title,
+        headerStyle: {
+            backgroundColor: "",
+        },
+        headerTintColor: Colors.primaryColor,
+    };
 };
 
 const styles = StyleSheet.create({
