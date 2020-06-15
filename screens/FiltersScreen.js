1da1ac52b12c14e9f980ff349e19207ec32e404a
bdafahim <badrudduza20@student.sust.edu>
2020-06-15 20:48:48 +0600

    Deleted some styles

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -9,13 +9,6 @@ const FilterScreen = (props) => {
     );
 };
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-});
+const styles = StyleSheet.create({});
 
 export default FilterScreen;
